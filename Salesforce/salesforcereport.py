2cd313ab2070f83f229954f486dde8a1b7ba3d69
tcfredericgilbert <frederic.gilbert@tc.tc>
2016-09-30 13:53:51 -0400

    Update salesforcereport.py

--- Salesforce/salesforcereport.py
+++ Salesforce/salesforcereport.py
@@ -1,11 +1,21 @@
 from simple_salesforce import Salesforce
 import requests
 import subprocess
+import csv
+import datetime
+
+# Get the current date and time
+now = datetime.datetime.now()
+now_str = now.strftime("%Y%m%d")
 
 sf = Salesforce(username='sgagnier@tc.tc', password='R1d1x2016', security_token='6Dm0v3MtZlrZgFF6QC6OaV4a')
 
 cookies = dict(sid=sf.session_id)
 r = requests.get("https://na4.salesforce.com/00O60000004kXAD?csv=1", cookies=cookies)
-with open('\Users\gilbertf\Downloads\SiteMapping_NEW.csv', 'w') as f:
+
+siteMapping_File= "\Users\gilbertf\Downloads\SiteMapping_"+str(datetime.datetime.now().strftime('%Y%m%d'))+".csv"
+
+with open(str(siteMapping_File), 'wb') as f:
      f.write(r.content)
-subprocess.Popen('\Users\gilbertf\Downloads\SiteMapping_NEW.csv',shell=True)
+     
+# subprocess.Popen(siteMapping_File,shell=True)
