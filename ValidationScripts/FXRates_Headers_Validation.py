a7b94e638d392154d7ceef105696631914de0bcb
tcfredericgilbert <frederic.gilbert@tc.tc>
2016-10-04 14:52:28 -0400

    Add files via upload

---
+++ ValidationScripts/FXRates_Headers_Validation.py
@@ -0,0 +1,44 @@
+import csv
+
+## File to validate :: Change name 
+FXRates_File = "FXRates.csv"
+
+## Companion Files :: Do not change except when new header row is modified
+## Script requires FXRates_Headers.csv and BQ_Headers.csv to be in same folder
+FXRates_Headers = "FXRates_Headers.csv"
+BQ_Headers = "FXRates_Headers.csv"
+
+with open(FXRates_File, "rb") as FXData:
+    reader = csv.reader(FXData)
+    FXData = reader.next()
+    rest = [row for row in reader]
+print FXRates_File,'=',FXData
+print
+
+with open(FXRates_Headers, "rb") as FXRates_Control:
+    reader = csv.reader(FXRates_Control)
+    FXRates_Control = reader.next()
+    rest = [row for row in reader]
+# print DFP_FieldControl,'=', FXRates_Control
+
+with open(BQ_Headers, "rb") as BQ_Control:
+    reader = csv.reader(BQ_Control)
+    BQ_Control = reader.next()
+    rest = [row for row in reader]
+# print BQ_FieldOutput,'=',BQ_Control
+
+set1, set2 = set(FXData), set(FXRates_Control)
+set3 = set(FXData), set(BQ_Control)
+
+[i for i in FXData if i not in set2]
+[i for i in FXRates_Control if i not in set1]
+[i for i in FXData if i not in set3]
+
+print 'FXRates Column(s) not from FXRates Control File=', ','.join([i for i in FXData if i not in FXRates_Control])
+print 'FXRAtes Column(s) missing in FXRates raw report=', ','.join([i for i in FXRates_Control if i not in BQ_Control and i not in FXData])
+
+print
+if len([i for i in FXData if i not in FXRates_Control]) <> 0:
+    print 'STOP processing:',FXRates_File
+else:    
+    print 'GO processing:',FXRates_File
